7da43ae7f1e4efa5e3385e977faac7b581bcdd64
WilliamKoza <william.koza@gmail.com>
2017-04-12 21:04:26 +0200

    feat(form): Add role selector on value accessor
    feat(form): Add role selector on value accessor

--- packages/forms/src/directives/select_multiple_control_value_accessor.ts
+++ packages/forms/src/directives/select_multiple_control_value_accessor.ts
@@ -70,7 +70,8 @@ abstract class HTMLCollection {
  */
 @Directive({
   selector:
-      'select[multiple][formControlName],select[multiple][formControl],select[multiple][ngModel]',
+      'select[multiple][formControlName],select[multiple][formControl],select[multiple][ngModel],' +
+      '[role="multiple"][formControlName],[role="multiple"][formControl],[role="multiple"][ngModel]',
   host: {'(change)': 'onChange($event.target)', '(blur)': 'onTouched()'},
   providers: [SELECT_MULTIPLE_VALUE_ACCESSOR]
 })
